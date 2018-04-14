61594bbe9d4bb98f051c91d5cff472907382431a
khalidnaser2323 <khalid.naser2323@gmail.com>
2018-04-14 06:07:11 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import * as functions from 'firebase-functions';
+const request = require('request');
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
+const authenticate = function (callback: (error, response, body) => void) {
+    let options = {
+        uri: 'https://accept.paymobsolutions.com/api/auth/tokens',
+        method: 'POST',
+        json: {
+            "username": "Chimpin",
+            "password": "FootPrints1234",
+            "expiration": "36000" // expiration in seconds Defaults to 1 hour
+        }
+    };
+    request(options, callback);
+}
+const createOrder = function (authenticationToken: string, amount: number, callback: (error, response, body) => void) {
+    let options = {
+        uri: 'https://accept.paymobsolutions.com/api/ecommerce/orders?token=' + authenticationToken,
+        method: 'POST',
+        json: {
+            "amount_cents": amount
+        }
+    };
+    request(options, callback);
+}
+
+export const pay = functions.https.onCall((data, context) => {
+    if (data.amount) {
+
+        authenticate((error, response, body) => {
+            if (!error && response.statusCode == 201) {
+                //here put what you want to do with the request
+                //  servicesResponse.send(response.body.token);
+                createOrder(response.body.token, data.amount, (createOrderError, createOrderResponse, createOrderBody) => {
+                    if (!createOrderError && createOrderResponse.id) {
+                        return { "orderId": createOrderResponse.id };
+                    }
+                    else {
+                        throw new functions.https.HttpsError('cancelled', error);
+                    }
+
+                });
+
+            }
+            else {
+                throw new functions.https.HttpsError('cancelled', error);
+
+            }
+        });
+    }
+    else {
+        throw new functions.https.HttpsError('invalid-argument', "Request must contain payment amount in cents", "Request must contain payment amount in cents");
+    }
+});
